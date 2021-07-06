967cfcd2f1aaa4b885f3d17aa5c3219771f784d2
Agbleze <62272559+agbleze@users.noreply.github.com>
2021-07-06 21:23:41 +0200

    Update server.R

--- USElections_shinyapp/server.R
+++ USElections_shinyapp/server.R
@@ -8,6 +8,58 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(socviz)
+
+theData <- reactive({
+    ## load dataset
+    election_dataset <- elections_historic
+    #View(election_dataset)
+    #piping the dataset to make it available to other functions without explicitly refering to it
+    election_dataset %>%
+        select(year, win_party, "President elected" = winner, votes, 
+               "Share_electoral_college_%" = ec_pct,
+               popular_pct, "Popular_vote_margin" = margin, turnout_pct, 
+               "Winner_electoral_college_vote" = ec_votes) %>%
+        filter(year == input$elect_yr[1], year == input$elect_yr[2])
+})
+
+server <- function(input, output, session){
+    # plot timeseries of voter turnout
+    output$timeseries <- renderPlot({
+        ggplot(data  = theData(), x = year, y = turnout_pct) + geom_line() + 
+            geom_smooth(method = loess())
+    }
+        
+    )
+    
+    # plot of unpopular type
+    output$unpopolarType <- renderText({
+        ifelse(election_dataset$popular_pct > 0.5, "Popular", "Unpopular")
+    }
+        
+    )
+    
+    # plot for party profile
+    output$partyprof <- renderPlot({
+        ggplot(data = theData()) + geom_bar(aes(win_party, fill = win_party), stat = count)
+    })
+    
+    # plot for turnout
+    output$ecTurnout <- renderValueBox({
+        yearSel <- select_yr.value
+        
+        turnout <- filter(elections_historic$year == yearSel)
+        turnoutData <- as.numeric(turnout$turnout_pct) * 100
+    })
+}
+
+
+
+
+
+
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
